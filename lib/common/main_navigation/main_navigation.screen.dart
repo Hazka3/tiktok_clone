b2af0e690a2bd508212feef2b7565bb5b71c36d4
Hazka3 <uryuryu.c@gmail.com>
2026-01-28 20:27:22 +0900

    feat(home/timeline): Implement infinite scroll on video-timline)

--- lib/common/main_navigation/main_navigation.screen.dart
+++ lib/common/main_navigation/main_navigation.screen.dart
@@ -4,6 +4,7 @@ import 'package:tiktok_clone/common/main_navigation/widgets/navigation_tab.dart'
 import 'package:tiktok_clone/common/main_navigation/widgets/post_video_button.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/videos/video_timeline_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -58,7 +59,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
         children: [
           Offstage(
             offstage: _selectedIndex != 0,
-            child: screens[_selectedIndex],
+            child: const VideoTimelineScreen(),
           ),
           Offstage(
             offstage: _selectedIndex != 1,
